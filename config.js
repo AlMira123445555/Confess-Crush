9eb9e14db525b20537d3f635d9295c960deddc64
AlMira123445555 <97288359+AlMira123445555@users.noreply.github.com>
2022-01-07 18:25:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
     titleWeb: "Gửi cho bạn Huyền <3",
     introTitle: 'Trần Thị Khánh Huyền',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `I love three things in this world: The Sun, the Moon and You. 
+The Sun for the morning, the Moon for the night. And you, FOREVER!`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    title: 'Bạn có muốn chúng ta sẽ bên nhau không🥰',
+    desc: 'không đồng ý mình buồn lắm đó ',
+    btnYes: 'Yêu QA',
+    btnNo: 'Không yêu QA :3',
+    question: 'Cảm ơn vì bạn đã chọn mình và bên cạnh mình nha',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Lý do bạn thích mình?',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messDesc: 'Mong rằng chúng ta sẽ sớm gặp nhau.',
+    btnAccept: 'Yêu bạn ',
+    messLink: 'https://www.facebook.com/messages/t/100071459343225
 }
